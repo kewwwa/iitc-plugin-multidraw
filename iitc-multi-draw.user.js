72deba40278e7fe6f37e2ab94e91d3c16a99d325
Thibaut Diseur <tdiseur@cameleongroup.com>
2017-05-10 15:25:21 +0200

    Toolbar

--- iitc-multi-draw.user.js
+++ iitc-multi-draw.user.js
@@ -28,62 +28,147 @@ function wrapper(plugin_info) {
     var setup = (function (window, undefined) {
         'use strict';
 
-        var firstPortal, secondPortal, currentPortal = null;
+        var plugin, actions,
+            firstPortal, secondPortal;
 
         if (typeof window.plugin !== 'function') window.plugin = function () { };
         window.plugin.multidraw = function () { };
-        window.plugin.multidraw.setup = setup;
+        plugin = window.plugin.multidraw;
+        plugin.setup = setup;
 
-        return setup;
+        return plugin.setup;
 
-        function setup() {
-            addHook('portalDetailsUpdated', portalSelected);
-        }
-
-        function portalSelected(data) {
-            var latlngs;
-
-            currentPortal = null;
-
-            if (!(data && data.portal)) return;
-
-            currentPortal = {
-                guid: data.guid,
-                ll: data.portal.getLatLng()
-            };
-
-            if (typeof firstPortal === typeof undefined) {
-                log('First portal selected');
-                firstPortal = currentPortal;
+        function toggleMenu() {
+            if (actions.classList.contains("active")) {
+                actions.classList.remove("active");
             }
             else {
-                latlngs = [];
-                latlngs.push(firstPortal.ll);
-
-                if (typeof secondPortal === typeof undefined) {
-                    log('Second portal selected');
-                    secondPortal = currentPortal;
-                }
-                else {
-                    log('New portal selected');
-                    latlngs.push(currentPortal.ll);
-                }
-
-                latlngs.push(secondPortal.ll);
-
-                window.map.fire('draw:created', {
-                    layer: L.geodesicPolyline(latlngs, window.plugin.drawTools.lineOptions),
-                    layerType: 'polyline'
-                });
-
-                if (!window.map.hasLayer(window.plugin.drawTools.drawnItems)) {
-                    window.map.addLayer(window.plugin.drawTools.drawnItems);
-                }
+                actions.classList.add("active");
             }
+        }
+
+        function selectFirstPortal() {
+            log('First portal selected');
+            toggleMenu();
+
+            firstPortal = getPortalSelected();
+            if (!firstPortal) return;
+
+            draw();
+        }
+
+        function selectSecondPortal() {
+            var latlngs;
+            log('Second portal selected');
+            toggleMenu();
+
+            secondPortal = getPortalSelected();
+            if (!secondPortal) return;
+
+            draw();
+        }
+
+        function selectOtherPortal() {
+            var portal;
+            log('Other portal selected');
+            toggleMenu();
+
+            portal = getPortalSelected();
+            if (!portal) return;
+
+            draw(portal);
+        }
+
+        function draw(portal) {
+            var latlngs;
+
+            if (!(firstPortal && secondPortal)) return;
+
+            latlngs = [];
+            latlngs.push(firstPortal.ll);
+            if (portal) latlngs.push(portal.ll);
+            latlngs.push(secondPortal.ll);
+
+            window.map.fire('draw:created', {
+                layer: L.geodesicPolyline(latlngs, window.plugin.drawTools.lineOptions),
+                layerType: 'polyline'
+            });
+
+            if (!window.map.hasLayer(window.plugin.drawTools.drawnItems)) {
+                window.map.addLayer(window.plugin.drawTools.drawnItems);
+            }
+        }
+
+        function getPortalSelected(data) {
+            if (!(selectedPortal && portals[selectedPortal])) return;
+
+            return {
+                guid: selectedPortal,
+                ll: portals[selectedPortal].getLatLng()
+            };
         }
 
         function log(message) {
             console.log('Multi draw: ' + message);
+        }
+
+        function setup() {
+            var parent, control, section, toolbar,
+                button,
+                firstPortal, secondPortal, otherPortal,
+                firstPortalLink, secondPortalLink, otherPortalLink;
+
+            $('<style>').prop('type', 'text/css')
+                .html('.leaflet-draw-actions.active{display: block;}.leaflet-control-multidraw a.leaflet-multidraw-edit-edit {background-image: url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMCIgaGVpZ2h0PSIzMCI+Cgk8ZyBzdHlsZT0iZmlsbDojMDAwMDAwO2ZpbGwtb3BhY2l0eTowLjQ7c3Ryb2tlOm5vbmUiPgoJCTxwYXRoIGQ9Ik0gNiwyNCAyNCwyNCAxNSw2IHoiLz4KCQk8cGF0aCBkPSJNIDYsMjQgMjQsMjQgMTUsMTIgeiIvPgoJCTxwYXRoIGQ9Ik0gNiwyNCAyNCwyNCAxNSwxOCB6Ii8+Cgk8L2c+Cjwvc3ZnPgo=");}')
+                .appendTo('head');
+
+            button = document.createElement("a");
+            button.className = "leaflet-multidraw-edit-edit";
+            button.addEventListener("click", toggleMenu, false);
+            button.title = 'Draw multi links';
+
+            toolbar = document.createElement("div");
+            toolbar.className = "leaflet-draw-toolbar leaflet-bar";
+            toolbar.appendChild(button);
+
+            firstPortalLink = document.createElement("a");
+            firstPortalLink.innerText = "1";
+            firstPortalLink.title = 'Select first portal';
+            firstPortalLink.addEventListener("click", selectFirstPortal, false);
+            firstPortal = document.createElement("li");
+            firstPortal.appendChild(firstPortalLink);
+
+            secondPortalLink = document.createElement("a");
+            secondPortalLink.innerText = "2";
+            secondPortalLink.title = 'Select second portal';
+            secondPortalLink.addEventListener("click", selectSecondPortal, false);
+            secondPortal = document.createElement("li");
+            secondPortal.appendChild(secondPortalLink);
+
+            otherPortalLink = document.createElement("a");
+            otherPortalLink.innerText = "N";
+            otherPortalLink.title = 'Select other portal';
+            otherPortalLink.addEventListener("click", selectOtherPortal, false);
+            otherPortal = document.createElement("li");
+            otherPortal.appendChild(otherPortalLink);
+
+            actions = document.createElement("ul");
+            actions.className = "leaflet-draw-actions leaflet-draw-actions-top";
+            actions.appendChild(firstPortal);
+            actions.appendChild(secondPortal);
+            actions.appendChild(otherPortal);
+
+            section = document.createElement("div");
+            section.className = "leaflet-draw-section";
+            section.appendChild(toolbar);
+            section.appendChild(actions);
+
+            control = document.createElement("div");
+            control.className = "leaflet-control-multidraw leaflet-draw leaflet-control";
+            control.appendChild(section);
+
+            parent = $(".leaflet-top.leaflet-left", window.map.getContainer());
+            parent.append(control);
         }
     })(window);
     // PLUGIN END //////////////////////////////////////////////////////////
@@ -101,4 +186,3 @@ var info = {};
 if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) info.script = { version: GM_info.script.version, name: GM_info.script.name, description: GM_info.script.description };
 script.appendChild(document.createTextNode('(' + wrapper + ')(' + JSON.stringify(info) + ');'));
 (document.body || document.head || document.documentElement).appendChild(script);
-
